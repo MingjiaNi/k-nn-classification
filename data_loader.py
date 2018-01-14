3f0c31c321f7aa376a7451355c12f26175bb9d69
Nafseer Kadiyaravida <nafseerk@gmail.com>
2018-01-14 13:02:45 -0500

    refactoring and added jupyter notebook

--- data_loader.py
+++ data_loader.py
@@ -1,42 +1,110 @@
 import pandas as pd
-import pprint
+from functools import reduce
 
+class DataLoader:
+    """
+    Class that helps in loading dataset. Assumes attributes and labels are in seperate file
+    """
 
-def build_ith_dataset(train_attributes_file, train_label_file):
-    dataset_i = {5: [], 6: []}
+    num_files = 10
 
-    # Load attributes file and label file
-    df_attributes = pd.read_csv(train_attributes_file, header=None)
-    df_label = pd.read_csv(train_label_file, header=None)
+    @classmethod
+    def load_dataset(cls, attributes_file, labels_file):
+        """
+        :param attributes_file: file path of attributes
+        :param labels_file: file path of correspnding labels
+        :return a 2-tuple of dataframes of attributes and labels
+        Loads data from single attributes, labels file pair
+        """
 
-    if len(df_attributes) != len(df_label):
-        raise ValueError('attributes and label file does not match')
+        # Load attributes file and value file
+        df_attributes = pd.read_csv(attributes_file, header=None, dtype=int)
+        df_labels = pd.read_csv(labels_file, header=None, dtype=float)
 
-    for index, row in df_attributes.iterrows():
-        label = df_label.iloc[[index]].values[0][0]
-        if label in [5, 6]:
-            dataset_i[label].append(list(row.values))
-        else:
-            raise ValueError('Unknown label encountered')
+        if len(df_attributes) != len(df_labels):
+            raise ValueError('count mismatch in attributes and labels file')
 
-    return dataset_i
+        return df_attributes, df_labels
 
+    @classmethod
+    def load_full_dataset(cls, dataset_root_directory):
+        """
+        :param dataset_root_directory: root directory containing all dataset files
+        :return a list of 2-tuples of dataframes of attributes and labels
+        Loads data from a collection of files
+        """
 
-def build_dataset(dataset_path, k=10):
+        full_dataset = []
+        for i in range(1, DataLoader.num_files + 1):
+            attributes, labels = DataLoader.load_dataset(
+                                    dataset_root_directory + '/data' + str(i) + '.csv',
+                                    dataset_root_directory + '/labels' + str(i) + '.csv'
+                                    )
+            full_dataset.append((attributes, labels))
+        return full_dataset
 
-    dataset = []
-    for i in range(1, k + 1):
-        ith_dataset = build_ith_dataset(dataset_path + '/data' + str(i) + '.csv',
-                                        dataset_path + '/labels' + str(i) + '.csv')
-        dataset.append(ith_dataset)
+    @classmethod
+    def load_with_test_data(cls, dataset_root_directory, split_ratio=0.1):
 
-    return dataset
+        if not 0 < split_ratio < 1:
+            raise ValueError('Split ratio should be in (0,1)')
+
+        # Concatenate all test files data together
+        test_files_count = int(split_ratio * DataLoader.num_files)
+        attributes_group = []
+        labels_group = []
+        for i in range(1, test_files_count + 1):
+            test_attrs, test_labels = DataLoader.load_dataset(
+                                          dataset_root_directory + '/data' + str(i) + '.csv',
+                                          dataset_root_directory + '/labels' + str(i) + '.csv')
+            attributes_group.append(test_attrs)
+            labels_group.append(test_labels)
+
+        test_attrs = pd.concat(attributes_group, ignore_index=True)
+        test_labels = pd.concat(labels_group, ignore_index=True)
+
+        # Treat remaining files as training files
+        train_dataset = []
+        for i in range(test_files_count + 1, DataLoader.num_files + 1):
+            attributes, labels = DataLoader.load_dataset(
+                dataset_root_directory + '/data' + str(i) + '.csv',
+                dataset_root_directory + '/labels' + str(i) + '.csv'
+            )
+            train_dataset.append((attributes, labels))
+
+        return train_dataset, test_attrs, test_labels
 
 
 if __name__ == '__main__':
 
-    dataset_ith = build_ith_dataset('knn-dataset/data1.csv', 'knn-dataset/labels1.csv')
-    pprint.pprint(dataset_ith)
+    part_train_attrs, part_train_labels = DataLoader.load_dataset(
+                                              'knn-dataset/data1.csv',
+                                              'knn-dataset/labels1.csv'
+                                          )
+    print('='*5 + 'Training data from single file' + '='*5)
+    print('Attributes = ', part_train_attrs.shape)
+    print('Labels = ', part_train_labels.shape)
+    print()
 
-    full_dataset = build_dataset('./knn-dataset')
-    print(len(full_dataset))
+    full_dataset = DataLoader.load_full_dataset('./knn-dataset')
+    print('=' * 15 + 'Full dataset' + '=' * 15)
+    attr_count = labels_count = 0
+    for ds in full_dataset:
+        attr_count += ds[0].shape[0]
+        labels_count += ds[1].shape[0]
+    print('Attributes = ', attr_count)
+    print('Labels = ', labels_count)
+    print()
+
+    train_dataset, test_attrs, test_labels = DataLoader.load_with_test_data(
+                                                            './knn-dataset',
+                                                            split_ratio=0.1)
+    print('=' * 5 + 'Train-Test split' + '=' * 5)
+    attr_count = labels_count = 0
+    for ds in train_dataset:
+        attr_count += ds[0].shape[0]
+        labels_count += ds[1].shape[0]
+    print('Train Attributes = ', attr_count)
+    print('Train Labels = ', labels_count)
+    print('Test Attributes = ', test_attrs.shape)
+    print('Test Labels = ', test_labels.shape)
--- knn_classifier.py
+++ knn_classifier.py
@@ -1,130 +1,164 @@
 import numpy as np
 from collections import Counter
-from data_loader import build_dataset
+from data_loader import DataLoader
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
-def k_nearest_neighbours(train_set, predict, k=3):
-    """
-    :param train_set: A list of training data subsets
-        each subset is a dictionary with two keys - 5 and 6
-        and value is a list of all data-points having that label
-    :param predict: a single data-point whose k nearest neighbors must be determined
-    :param k: number of neighbors
-    :return: a list of size of of 3-tuples (distance, neighbour data-point, label)
-    """
-    distances = []
-    for subset in train_set:
-        for label in subset:
-            for x in subset[label]:
-                euclidean_distance = np.linalg.norm(np.array(x) - np.array(predict))
-                distances.append((euclidean_distance, x, label))
+class KNearestNeighbours:
 
-    neighbours = sorted(distances)[:k]
-    return neighbours
+    def __init__(self):
+        self.max_k = 30
+        self.best_k = None
+        self.best_acc = None
+        self.k_values = []
+        self.accuracy_values = []
+        self.distance_cache = {}
+        self.k_neighbours = {}
 
+    def get_euclidean_distance(self, x1, x2):
 
-def knn_classify(train_set, predict, k=3):
-    """
-    :param train_set: A list of training data subsets
-        each subset is a dictionary with two keys - 5 and 6
-        and value is a list of all data-points having that label
-    :param predict: a single data-point whose k nearest neighbors must be determined
-    :param k: number of neighbors
-    :return: the predicted label of the single target data-point
-    """
+        if np.array_equal(x1, x2):
+            return 0.0
+        else:
+            key = (tuple(x1), tuple(x2))
+            if key not in self.distance_cache:
+                self.distance_cache[key] = np.linalg.norm(x1 - x2)
+            return self.distance_cache[key]
 
-    neighbours = k_nearest_neighbours(train_set, predict, k)
-    neighbour_classes = [i[2] for i in neighbours]
-    selected_class = Counter(neighbour_classes).most_common(1)[0][0]
-    return selected_class
+    def learn(self, full_dataset, k=None, verbose=False):
 
+        # If k is not provided then k is learned
+        if k is None:
+            self.best_k, self.best_acc = self.learn_best_k(full_dataset, verbose=verbose)
 
-def knn_classify_with_cross_validation(train_set, test_set, k=3):
-    """
-    :param train_set: A list of training data subsets
-        each subset is a dictionary with two keys - 5 and 6
-        and value is a list of all data-points having that label
-    :param test_set: A single subset of the training data used for cross validation
-    :param k: number of neighbors
-    :return: the predicted labels of all the data-points in the test set and the accuracy
-    """
+    def k_nearest_neighbours(self, train_set, predict):
+        """
+        :param train_set: A list of training data subsets
+            each subset is 2-tuple of attributes and corresponding labels
+        :param predict: a single data-point whose k nearest neighbors must be determined
+        :param k: number of neighbors
+        """
+        row_key = tuple(predict)
+        if row_key not in self.k_neighbours:
+            neighbours = []
+            for train_attrs, train_labels in train_set:
+                for index, row in train_attrs.iterrows():
+                    euclidean_distance = self.get_euclidean_distance(row.values, predict)
+                    neighbours.append((euclidean_distance, train_labels.iat[index, 0]))
 
-    predicted_labels = []
-    correct = 0
-    incorrect = 0
-    for true_label in test_set:
-        for x in test_set[true_label]:
-            predicted_label = knn_classify(train_set, x, k)
+            neighbours = sorted(neighbours, key=lambda x: x[0])[:self.max_k]
+            self.k_neighbours[row_key] = neighbours
+        return self.k_neighbours[row_key]
+
+    def knn_classify_point(self, train_set, predict, k=3):
+        """
+        :param train_set: A list of training data subsets
+            each subset is 2-tuple of attributes and corresponding labels
+        :param predict: a single data-point whose class must be determined
+        :param k: number of neighbors
+        :return: the predicted label of the single target data-point
+        """
+        neighbours = self.k_nearest_neighbours(train_set, predict)
+        neighbour_classes = [i[1] for i in neighbours][:k]
+        selected_class = Counter(neighbour_classes).most_common(1)[0][0]
+        return selected_class
+
+    def classify(self, train_set, test_attrs, true_values=None, k=None):
+        """
+        :param train_set: A list of training data subsets
+            each subset is 2-tuple of attributes and corresponding labels
+        :param test_attrs: test data for classification
+        :param true_values: true_labels of the test set
+        :param k: number of neighbors
+        :return: the predicted labels of all the data-points in the test set and the accuracy
+        """
+
+        if true_values is not None:
+            if len(test_attrs) != len(true_values):
+                raise ValueError('count mismatch in attributes and labels')
+
+        if k is None:
+            k = self.best_k
+
+        predicted_labels = []
+        correct = 0
+        incorrect = 0
+        for index, row in test_attrs.iterrows():
+            predicted_label = self.knn_classify_point(train_set, row.values, k=k)
             predicted_labels.append(predicted_label)
-            if predicted_label == true_label:
-                correct += 1
-            else:
-                incorrect += 1
+            if true_values is not None:
+                true_label = true_values.iat[index, 0]
+                if predicted_label == true_label:
+                    correct += 1
+                else:
+                    incorrect += 1
 
-    accuracy = correct / (correct + incorrect) * 100
+        accuracy = None
+        if true_values is not None:
+            accuracy = correct / (correct + incorrect) * 100
 
-    return predicted_labels, accuracy
+        predicted_labels = pd.DataFrame(np.array(predicted_labels))
+        return predicted_labels, accuracy
 
+    def t_fold_cross_validation(self, full_dataset, t=10, k=3):
+        avg_accuracy = 0
+        for i in range(t):
+            test_attrs, test_labels = full_dataset.pop(0)
+            accuracy = self.classify(full_dataset, test_attrs, true_values=test_labels, k=k)[1]
+            full_dataset.append((test_attrs, test_labels))
+            avg_accuracy += accuracy
+        return avg_accuracy / 10
 
-def ten_fold_cross_validation(full_dataset, num_neighbours=3):
+    def learn_best_k(self, full_dataset, verbose=False):
 
-    avg_accuracy = 0
-    for i in range(10):
-        test_set = full_dataset.pop(0)
-        accuracy = knn_classify_with_cross_validation(full_dataset, test_set, k=num_neighbours)[1]
-        #print('Experiment %d accuracy = %.2f %%' % (i+1, accuracy))
-        full_dataset.append(test_set)
-        avg_accuracy += accuracy
-
-    return avg_accuracy / 10
-
-
-k_values = []
-accuracy_values = []
-
-
-def get_best_k(full_dataset):
-
-    for k in range(1, 31):
-        accuracy = ten_fold_cross_validation(full_dataset, num_neighbours=k)
-        print('At k = %d Accuracy = %.2f' % (k, accuracy))
-        k_values.append(k)
-        accuracy_values.append(accuracy)
-
-    best_acc = max(accuracy_values)
-    best_k = k_values[accuracy_values.index(best_acc)]
-
-    return best_k, best_acc
+        self.k_values = []
+        self.accuracy_values = []
 
 
-def plot_accuracy():
+        print('Learning in progress...')
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    line, = ax.plot(k_values, accuracy_values,)
+        for k in range(1, self.max_k + 1):
+            accuracy = self.t_fold_cross_validation(full_dataset, t=10, k=k)
+            if verbose:
+                print('At k = %d Accuracy = %.3f' % (k, accuracy))
+            self.k_values.append(k)
+            self.accuracy_values.append(accuracy)
 
-    ymax = max(accuracy_values)
-    xpos = accuracy_values.index(ymax)
-    xmax = k_values[xpos]
+        self.best_acc = max(self.accuracy_values)
+        self.best_k = self.k_values[self.accuracy_values.index(self.best_acc)]
 
-    ax.annotate('max at k = %d with %.2f acc' % (xmax, ymax), xy=(xmax, ymax), xytext=(xmax + 3, ymax + 1),
-                arrowprops=dict(facecolor='black', shrink=0.05),
-                )
+        if verbose:
+            print('Learned value of k =', self.best_k)
 
-    plt.xlabel('k')
-    plt.ylabel('Accuracy(%)')
-    plt.title('Test accuracy vs k')
-    plt.show()
-    plt.savefig("output.png")
+        return self.best_k, self.best_acc
+
+    def plot_accuracy(self):
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(self.k_values, self.accuracy_values, '.-')
+
+        ymax = self.best_acc
+        xmax = self.best_k
+
+        ax.annotate('max at k = %d, acc = %.3f %%' % (xmax, ymax), xy=(xmax, ymax), xytext=(xmax + 3, ymax + 1),
+                    arrowprops=dict(facecolor='black', shrink=0.05),
+                    )
+        ax.scatter([xmax], [ymax], c='r')
+        plt.xlabel('k')
+        plt.ylabel('Accuracy(%)')
+        plt.title('Accuracy vs k')
+        plt.show()
 
 
 if __name__ == '__main__':
-    dataset = build_dataset('./knn-dataset')
-    predictions, acc = knn_classify_with_cross_validation(dataset[:-1], dataset[-1])
-    print(predictions)
+    dataset = DataLoader.load_full_dataset('./knn-dataset')
+    model = KNearestNeighbours()
+    predictions, acc = model.classify(dataset[:-1], dataset[-1][0], true_values=dataset[-1][1])
+    #print(predictions)
     print(acc)
-    print(ten_fold_cross_validation(dataset))
+    #print(ten_fold_cross_validation(dataset))
 
-    print(get_best_k(dataset))
-    plot_accuracy()
+    print(model.learn_best_k(dataset, verbose=True))
+    model.plot_accuracy()
